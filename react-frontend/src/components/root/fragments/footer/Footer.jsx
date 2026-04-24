7346891b6cc085ff39856ba853f29b3fd663ae05
SPS <123806275+spspu@users.noreply.github.com>
2026-04-24 18:15:49 +0530

    commit

---
+++ react-frontend/src/components/root/fragments/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer = (props) => {
+  return (
+    <footer className={props.class}>
+      &copy; New York 2026. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
